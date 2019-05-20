b61139a0012e9644393b1c035433c4e65e448a27
lonzohope <hopemutheu@gmail.com>
2019-05-20 12:14:42 +0300

    made migrations

--- gram/models.py
+++ gram/models.py
@@ -4,9 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Profile(models.Model):
-   profile_photo = models.ImageField(upload_to=)
-   bio = models.CharField(max_length =30)
+    profile_photo = models.ImageField(upload_to =30)
+    bio = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.name
 
+    def save_profile(self):
+        self.save()
+   
 
 class Image(models.Model):
     image = models.ImageField(upload_to="images/")
@@ -15,5 +21,29 @@ class Image(models.Model):
     image_profile = models.ForeignKey(Profile)
     image_comments =  models.CharField(max_length =30)
     image_likes = models.IntegerField
+   
 
+    def __str__(self):
+        return self.image_name
 
+    class Meta:
+        ordering = ['image']
+
+    def save_image(self):
+        self.save()
+
+    def delete_iamge(self):
+        self.save()
+
+    def update_caption(self):
+        self.caption()
+
+    @classmethod
+    def get_image_by_id(cls):
+        images = cls.objects.get(pk=id)
+        return images
+        
+    @classmethod
+    def search_by_image_profile(cls,search_item):
+        images = cls.objects.filter(image_icontains=search_term)
+        return images
