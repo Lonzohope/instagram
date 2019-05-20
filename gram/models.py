482023080bfa347f09cf26f1f1035ec6164d794c
lonzohope <hopemutheu@gmail.com>
2019-05-20 11:19:18 +0300

    created image model

--- gram/models.py
+++ gram/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to="images/")
+    
