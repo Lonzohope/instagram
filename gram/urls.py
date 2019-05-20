9160b14cf7cda72062a85c5a9ff56267a1de54e8
lonzohope <hopemutheu@gmail.com>
2019-05-20 14:58:46 +0300

    import static

--- gram/urls.py
+++ gram/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
+
+
