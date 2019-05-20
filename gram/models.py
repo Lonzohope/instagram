9d8a411a739c44f4ecbf438a740f98347d397c34
lonzohope <hopemutheu@gmail.com>
2019-05-20 11:37:45 +0300

    created a profile model

--- gram/models.py
+++ gram/models.py
@@ -3,7 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Profile(models.Model):
+   profile_photo = models.ImageField(upload_to=)
+   bio = models.CharField(max_length =30)
+
 
 class Image(models.Model):
     image = models.ImageField(upload_to="images/")
-    
+    image_name = models.CharField(max_length =30)
+    image_caption = models.CharField(max_length =30)
+    image_profile = models.ForeignKey(Profile)
+    image_comments =  models.CharField(max_length =30)
+    image_likes = models.IntegerField
+
+
