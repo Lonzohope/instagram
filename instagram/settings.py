812be742e1d4610fb98fa20790d228e3990da662
lonzohope <hopemutheu@gmail.com>
2019-05-22 15:34:07 +0300

    deploying

--- instagram/settings.py
+++ instagram/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+DATABASES = { 'default' : dj_database_url.config() }
 
 # DATABASES = {
 #     'default': {
