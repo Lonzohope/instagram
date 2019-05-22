635e293bbe65397b9977bb1e156982ae7b7f227e
lonzohope <hopemutheu@gmail.com>
2019-05-22 15:28:21 +0300

    deploying

--- instagram/settings.py
+++ instagram/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(prod_db)
+DATABASES['default'].update(prod_db)
 
 # DATABASES = {
 #     'default': {
