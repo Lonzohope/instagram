45798dcfc4f71c7c8151e708c48b9eca5c3f8d15
lonzohope <hopemutheu@gmail.com>
2019-05-22 15:35:48 +0300

    deploying

--- instagram/settings.py
+++ instagram/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
